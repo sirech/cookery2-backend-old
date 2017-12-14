091eba7d09865ef9b591048d23edf794ca0d3793
Mario Fernandez <mario.fernandez@thoughtworks.com>
2017-12-14 21:41:20 +0100

    Update gems

--- spec/factories/recipe.rb
+++ spec/factories/recipe.rb
@@ -6,21 +6,21 @@ FactoryGirl.define do
 
     trait :with_categories do
       after(:build) do |recipe|
-        count = Random.rand(3) + 1
+        count = Random.rand(1..3)
         count.times { recipe.categories << FactoryGirl.build(:category) }
       end
     end
 
     trait :with_ingredients do
       after(:build) do |recipe|
-        count = Random.rand(6) + 1
+        count = Random.rand(1..6)
         count.times { recipe.quantities << FactoryGirl.build(:quantity) }
       end
     end
 
     trait :with_steps do
       after(:build) do |recipe|
-        count = Random.rand(3) + 1
+        count = Random.rand(1..3)
         count.times { recipe.steps << FactoryGirl.build(:step) }
       end
     end
